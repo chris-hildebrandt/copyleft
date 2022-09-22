1c88353c9699cb12539e5448b1cef84ae37ea307
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-21 19:11:35 -0600

    drawer model up

---
+++ copyleft/Models/Drawer.cs
@@ -0,0 +1,10 @@
+namespace copyleft.Models
+{
+  public class Drawer
+    {
+    public string Description { get; set; }
+    public string CoverImg { get; set; }
+    public string Name { get; set; }
+    public string CreatorId { get; set; }
+  }
+}
