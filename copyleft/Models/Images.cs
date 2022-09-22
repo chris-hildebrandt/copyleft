f3b3f9b65edb6407fbb77ebd07f54a434497677f
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-21 19:07:37 -0600

    images model up

---
+++ copyleft/Models/Images.cs
@@ -0,0 +1,10 @@
+namespace copyleft.Models
+{
+  public class Images
+  {
+    public string Title { get; set; }
+    public string Image { get; set; }
+    public int DrawerImageId { get; set; }
+    public string CreatorId { get; set; }
+  }
+}
--- copyleft/dbSetup.sql
+++ copyleft/dbSetup.sql
@@ -7,6 +7,36 @@ CREATE TABLE IF NOT EXISTS accounts(
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
 
+-- images
 CREATE TABLE IF NOT EXISTS images(
-  id AUTO_INCREMENT
-)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  image VARCHAR(500) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+-- drawers
+
+CREATE TABLE IF NOT EXISTS drawers(
+  drawerId INT AUTO_INCREMENT PRIMARY KEY,
+  description VARCHAR(500),
+  coverImg VARCHAR(500),
+  name VARCHAR(100) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+-- many to many relationship
+CREATE TABLE IF NOT EXISTS drawerImages(
+  drawerImageId INT AUTO_INCREMENT PRIMARY KEY,
+  drawerId INT NOT NULL,
+  imageId INT NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+insert into drawerImages
+(drawerId, imageId, creatorId)
+VALUES
+(2, 7,"62fead19fda8e818d13a81db")
